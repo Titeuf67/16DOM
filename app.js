9ccd0a4685100525842a65ad32e39a1bd6ed595b
Titeuf67 <christian.fillette@gmail.com>
2022-06-13 15:27:57 +0200

    13/06

---
+++ app.js
@@ -0,0 +1,48 @@
+// exo 8  condition
+
+// for (let i = 0; i <= 20; i += 2) {
+//     if (i == 10) {
+//         console.log('Je suis a arrivé à 10');
+//     }
+//     else
+//         console.log(i)
+// }
+
+//exo 9 selection aleatoire d'un chiffre pair ou impair
+
+// var num;
+
+// function getRandomInt(max) {
+//     num = Math.floor(Math.random() * max);
+
+//     console.log(num);
+//     return num
+// };
+
+// getRandomInt(10)
+
+// if(num %2 == 0 ){
+// console.log("paire");
+// }else {
+// console.log('impaire');
+// }
+
+// exo 10
+
+var num;
+
+function getRandomInt(max) {
+    num = Math.floor(Math.random() * max);
+
+    console.log(num);
+    return num
+};
+
+getRandomInt(10)
+
+if(num %2 == 0 ){
+    console.log();
+    }else {
+    console.log('impaire');
+    }
+    
